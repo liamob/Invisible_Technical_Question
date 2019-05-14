afc21d79bb1b29a7ddc0b1329061588883147d70
liamob <liamob@umich.edu>
2019-05-14 15:34:25 -0700

    Fixed some variable names and comments

--- Inv_Tech_Question.js
+++ Inv_Tech_Question.js
@@ -4,14 +4,14 @@
 //
 //
 //INSTRUCTIONS:
-//FIRST, change the PATH of the "newfile" const on line 14 to the path where you have latinmap.js stored on your computer.
+//FIRST, change the PATH of the "latinmapfile" const on line 14 to the path where you have latinmap.js stored on your computer.
 //
 //To run program on the command line: $ node Inv_Tech_Question.js -c 1
 //If you want to run the program with a different test array, change the last number above. I have 5 test arrays. 
 //
 //To run the program with your own test array, create an array of postal codes and city names. Then, run the function, array_of_citys_or_post//alcodes(array), and put your array in the argument.
 
-const newfile = require("/Users/LiamOBrien/Desktop/invisible/Invisible_Technical_Question/latinmap.js");//include map that changes undifined letters like 'ã' to readable letters like 'a'.
+const latinmapfile = require("/Users/LiamOBrien/Desktop/invisible/Invisible_Technical_Question/latinmap.js");//include map that changes undifined letters like 'ã' to readable letters like 'a'.
 const request = require('request');
 const DSrequest = require('request');
 const zipcodes = require('zipcodes');
@@ -19,60 +19,64 @@ const zipcodes = require('zipcodes');
 //Function take a single input(either city or postal code) and returns time and weather.
 function time_and_weather(cityorzip){
   let output = ``;
-  let apiKey = 'c52d902070cf78c08d87733170517dca';
-  let darkskyapikey = '7a664d504e7b11c2da6e8fb7dd9081c5';
+  let apiKey = 'c52d902070cf78c08d87733170517dca'; //open weather map api key.
+  let darkskyapikey = '7a664d504e7b11c2da6e8fb7dd9081c5';//dark ski api key.
   let country = 0;
   let iszip = 0;//bool
   let cleancityorzip = cityorzip.latinise(); //clean the input to make sure it doesn't use any undefined symbols.
-  //Use openweathermap api to access latitude and longitude of inputed location. 
   let url = '';
-  if(isNaN(cityorzip)){
+  //check if input is postal code or city name. 
+  if(isNaN(cityorzip)) { //Use openweathermap api to access latitude and longitude of inputed location.
     iszip = 0;
     try {
       url = `http://api.openweathermap.org/data/2.5/weather?q=${cleancityorzip}&units=imperial&appid=${apiKey}`;//given city name
     }
     catch(err) {
-      console.log(`The city, ${cityorzip}, doesn't exist`);
+      console.log(`ERROR: The city, ${cityorzip}, doesn't exist`);// if city doesn't exist
       return;
     }
-  } else {
+  } 
+  else {
     iszip = 1;
     try {
       country = zipcodes.lookup(cleancityorzip).country;
       url = `http://api.openweathermap.org/data/2.5/weather?zip=${cleancityorzip},${country}&units=imperial&appid=${apiKey}`;//given zipcode
     }
-    catch(err){
-      console.log(`postal code, ${cityorzip}, doesn't exist`);
+    catch(err) {
+      console.log(`ERROR: Postal code, ${cityorzip}, doesn't exist`);// if postal code doesn't exist
       return;
     }
   }
-  //access open weather map api.
+  //access open weather map api using request functionality. 
   request(url, function (err, response, body) {
-    if(err){
-      console.log('error:', error);
-    } else {
+    if(err) {
+      console.log('error:', err);
+    }
+    else {
       let weather = JSON.parse(body);//parse open weathermap api. Use this data to get latitude and longitude. 
       let DS_url = ``;
       try {      
-        DS_url = `https://api.darksky.net/forecast/${darkskyapikey}/${weather.coord.lat},${weather.coord.lon}`;//use Dark sky api to get weather and time data.
+        DS_url = `https://api.darksky.net/forecast/${darkskyapikey}/${weather.coord.lat},${weather.coord.lon}`;//use Dark sky api to get             weather and time data.
       }
       catch(err){
         console.log(`ERROR: Dark sky api key has expired or the city/postal code, ${cityorzip}, doesn't exist`);
         return;
       }
+      //access open Dark Sky Weather api using request functionality.
       DSrequest(DS_url, function (DSerr, DSresponse, DSbody) {
         if(DSerr){
-          console.log('error:', DSerror);
-        } else { 
+          console.log('error:', DSerr);
+        } 
+        else { 
           let DSdata = JSON.parse(DSbody);//parse dark sky api data.
-          let TimeZone = DSdata.timezone;                             //
+          let TimeZone = DSdata.timezone;                             ///////////////
           var options = {                                             //  
             timeZone: `${TimeZone}`,                                  // Here I get the timezone from the dark sky api and I use the function
             hour: 'numeric', minute: 'numeric', second: 'numeric',    // "Intl.DateTimeFormat" to access the current time from the timezone 
           },                                                          // input.
           formatter = new Intl.DateTimeFormat([], options);           //
-          let formattedTime = formatter.format(new Date());           //
-          //log the time and weather to the console. If inputted data is bad (i.e. postal code or city doesn't exist), code will print error message and then continue reading next input. 
+          let formattedTime = formatter.format(new Date());           ///////////////
+          //Ahead, log the time and weather to the console. If inputted data is bad (i.e. postal code or city doesn't exist), code will print           error message and then continue reading next input. 
           try {
             if(iszip == 1){
               output = `At the ${cityorzip} postal code, the time is ${formattedTime} and the weather is ${DSdata.currently.temperature} degrees fahrenheit.`;
@@ -82,7 +86,7 @@ function time_and_weather(cityorzip){
             console.log(output);
           }
           catch(err){
-            console.log(`${cityorzip} is an undfefined input.`);// error message if city or postal code doesn't exist.
+            console.log(`ERROR: ${cityorzip} is an undfefined input.`);// error message if city or postal code doesn't exist.
             return;
           }
         }
@@ -99,6 +103,8 @@ function array_of_citys_or_postalcodes(array){
   }
 }
 /////////////////////Finished with code, below is test cases//////////////////////////
+
+//This fucntion runs 5 different test cases depending on what you input on the command line. 
 function test_func(){
   const argv = require('yargs').argv;
   let test1 = ['New York', '10005', 'Tokyo','São Paulo', 'Pluto'];
